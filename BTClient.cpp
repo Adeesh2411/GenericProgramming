075118ee8d56c3447aea7b6fc592673454dcdd1e
Adeesh <adeesh999@gmail.com>
2020-04-11 20:31:33 +0530

    Balanced tree done

--- BTClient.cpp
+++ BTClient.cpp
@@ -3,6 +3,7 @@
 #include<list>
 #include<initializer_list>
 #include "BinaryTree.hpp"
+#include<string>
 using namespace std;
 
 
@@ -10,7 +11,13 @@ using namespace std;
 int main(){
     vector<int> vec{2,3,4};
     int arr[4]={2,31,4,1};
-
-    BinaryTree<decltype(vec)> bt(vec);
-    bt.display();
+    string s="xyzabcd";
+    
+    BinaryTree<int, int> a(1,1);
+    a.insertNode(2,2);
+    a.insertNode(3,3);
+    a.insertNode(4,4);
+    a.display();
+    a.BalanceTree();
+    a.display();
 }
--- BinaryTree.hpp
+++ BinaryTree.hpp
@@ -1,12 +1,13 @@
 #include<iostream>
 #include<utility>
+#include<vector>
 using namespace std;
 
 //inner class of binary tree
 template<typename T>
 class TreeStruct{    
-    TreeStruct(const T &obj):data_(obj){}
-
+    TreeStruct(const T &obj, int r = -1):data_(obj), rank_(r){}
+    int rank_;
     TreeStruct *left, *right;
     T data_;
 
@@ -20,9 +21,9 @@ template<typename T1, typename T2 = typename T1::value_type>
 class BinaryTree{
     public:
         //constructors...
-        BinaryTree(){}
-        BinaryTree(int n, int r):size1_(n),rank_(r){}
-        BinaryTree(int n):size_(n){}
+        BinaryTree(const T1& unknownObject, int N):givenObject(unknownObject), rank_(N){
+           initialise();
+        }
         BinaryTree(const T1 &unknownObject):givenObject(unknownObject){
             createTree();
         }
@@ -30,19 +31,25 @@ class BinaryTree{
             deleteAll(Start);
             Start = nullptr;
         }
-
-        void display();
-        void display(TreeStruct<T2> *start);
+        //function declarations
+        void display();// to display the tree
+        void display(TreeStruct<T2> *start); // to display the tree 
         
-        void createTree();
-        void deleteAll(TreeStruct<T2>*);
-        void addNode(const T2 &a);
+        void createTree(); // Creating tree if collection is given
+        void deleteAll(TreeStruct<T2>*); // called by dtor at the end 
+        void addNode(const T2 &a); // adding node at the required position
+        void initialise();//Initialise the Start pointer for separate allocation
+        void insertNode(const T1& unknownObject, int rank);// inserting the given object at 
+                                                           //specified position for given rank     
+        void BalanceTree();//time complexity = O(n) and Space = O(n)
+        
+
     private:
-        int size_;
+        void storeNode(TreeStruct<T2>* St, vector<TreeStruct<T2>*> &v1);//store all the nodes
+        TreeStruct<T2>* BuildBalancedTree(vector<TreeStruct<T2>*> &v1, int start, int end);// AVL
         const T1& givenObject;
-        int size1_;
         int rank_;    
-        TreeStruct<T2> *Start;
+        TreeStruct<T2> *Start = nullptr;
 };
 
 //all the function definition
@@ -99,7 +106,7 @@ void BinaryTree<T1, T2>::display(TreeStruct<T2> *start){
 template<typename T1, typename T2>
 void BinaryTree<T1, T2>::display(){
     display(Start);
-    std::cout<<std::endl;
+    cout<<endl;
 }
 
 template<typename T1, typename T2>
@@ -110,4 +117,63 @@ void BinaryTree<T1, T2>::deleteAll(TreeStruct<T2>* Start){
         delete Start;
         Start = nullptr;
     }
+}
+
+template<typename T1, typename T2>
+void BinaryTree<T1, T2>::initialise(){
+    TreeStruct<T2> *Obj = new TreeStruct<T2>(givenObject, rank_);
+        Obj->left = Obj->right = nullptr;
+        Start = Obj;
+}
+
+
+template<typename T1, typename T2>
+void BinaryTree<T1, T2>::insertNode(const T1 &a, int rank){    
+    using innerType = T2;
+
+    TreeStruct<innerType> *temp = Start;
+    TreeStruct<innerType> *prev = nullptr;
+    while(temp){
+        prev = temp;
+        if(temp->rank_ < rank){
+            temp=temp->right;
+        }
+        else{
+            temp = temp->left;
+        }
+    }
+    if(prev && prev->rank_ < rank){
+        prev->right = new TreeStruct<innerType>(a, rank);
+    }
+    else {
+        prev->left = new TreeStruct<innerType>(a, rank);
+    }
+}
+
+template<typename T1, typename T2>
+void BinaryTree<T1, T2>::BalanceTree(){
+    vector<TreeStruct<T2>*> v;
+    storeNode(Start, v);
+    Start = BuildBalancedTree(v, 0, v.size()-1);
+}
+
+template<typename T1, typename T2>
+void BinaryTree<T1, T2>::storeNode(TreeStruct<T2>* root, vector<TreeStruct<T2>*> &v){
+    if(root){
+        storeNode(root->left, v);
+        v.push_back(root);
+        storeNode(root->right, v);
+    }
+}
+
+template<typename T1, typename T2>
+TreeStruct<T2>* BinaryTree<T1, T2>::BuildBalancedTree(vector<TreeStruct<T2>*> &v, int start, int end){
+    if(start<=end){
+        int mid = (start+end)/2;
+       
+        v[mid]->left = BuildBalancedTree(v, start, mid - 1);
+        v[mid]->right = BuildBalancedTree(v, mid+1, end); 
+        return v[mid];
+    }
+    return nullptr;
 }
